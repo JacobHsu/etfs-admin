e8c819e147126852a8ec5cda1fb6687e751c5353
jacobhsu <jacob.hsu.tw@gmail.com>
2021-02-07 16:44:38 +0800

    yarn build && gh-pages -d dist

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,11 @@ const path = require('path')
 // use administrator privileges to execute the command line.
 // For example, on Mac: sudo npm run / sudo yarn
 const devServerPort = 9527 // TODO: get this variable from setting.ts
-const mockServerPort = 9528 // TODO: get this variable from setting.ts
+const mockServerPort = 8888 // TODO: get this variable from setting.ts
 const name = 'Vue Typescript Admin' // TODO: get this variable from setting.ts
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-typescript-admin-template/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/etfs-admin/' : '/',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
