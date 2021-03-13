3235fd660b5e6e2363f815b866fe951255b48951
jacobhsu <jacob.hsu.tw@gmail.com>
2021-03-13 23:30:25 +0800

    DAL, DIS, DVN, ETN

--- mock/data/stock.ts
+++ mock/data/stock.ts
@@ -3,7 +3,12 @@ export const stock = [
   { id: 'AG', tag: '白銀開採', category: '資源', name: 'First Majestic Silver Corp', etf: 'AG', issuer: '', star: 0, type: 'stock' },
   { id: 'AMD', tag: '超微半導體', category: '科技', name: 'Advanced Micro Devices Inc', etf: 'AMD', issuer: '', star: 0, type: 'stock' },
   { id: 'API', tag: '聲網公司', category: '科技', name: 'Agora, Inc.', etf: 'API', issuer: '', star: 0, type: 'stock' },
+  { id: 'BABA', tag: '阿里巴巴', category: '電商', name: 'Alibaba Group Holding Limited', etf: 'BABA', issuer: '', star: 0, type: 'stock' },
   { id: 'BNGO', tag: '基因組分析', category: '生技', name: 'Bionano Genomics, Inc.', etf: 'BNGO', issuer: '', star: 0, type: 'stock' },
   { id: 'CHWY', tag: '寵物', category: '零售', name: 'Chewy, Inc.', etf: 'CHWY', issuer: '', star: 0, type: 'stock' },
+  { id: 'DAL', tag: '達美航空', category: '航空', name: 'Delta Air Lines Inc', etf: 'DAL', issuer: '', star: 0, type: 'stock' },
+  { id: 'DIS', tag: '迪士尼', category: '媒體', name: 'Walt Disney Co', etf: 'DIS', issuer: '', star: 0, type: 'stock' },
+  { id: 'DVN', tag: '德文能源', category: '能源', name: 'Devon Energy Corp', etf: 'DVN', issuer: '', star: 0, type: 'stock' },
+  { id: 'ETN', tag: '電源管理', category: '科技', name: 'Eaton Corporation, PLC', etf: 'ETN', issuer: '', star: 0, type: 'stock' },
   { id: 'LMND', tag: 'AI保險', category: '保險', name: 'Lemonade, Inc.', etf: 'LMND', issuer: '', star: 0, type: 'stock' }
 ]
